8fa6aaa57567887c2eba224e35c3009accebb8cf
Ehren Kret <ehren@signal.org>
2022-02-25 18:05:25 -0600

    Add test to ensure group logs aren't returned to banned members

--- src/test/java/org/signal/storageservice/controllers/GroupsControllerBannedMembersTest.java
+++ src/test/java/org/signal/storageservice/controllers/GroupsControllerBannedMembersTest.java
@@ -1,16 +1,24 @@
 package org.signal.storageservice.controllers;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyBoolean;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.ArgumentMatchers.isA;
+import static org.mockito.Mockito.when;
 
 import com.google.protobuf.ByteString;
 import java.security.SecureRandom;
 import java.util.Base64;
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
 import javax.ws.rs.core.Response;
 import org.junit.Test;
 import org.signal.storageservice.providers.ProtocolBufferMediaType;
 import org.signal.storageservice.storage.protos.groups.AccessControl;
 import org.signal.storageservice.storage.protos.groups.Group;
+import org.signal.storageservice.storage.protos.groups.GroupChanges.GroupChangeState;
 import org.signal.storageservice.util.AuthHelper;
 
 public class GroupsControllerBannedMembersTest extends BaseGroupsControllerTest {
@@ -51,6 +59,36 @@ public class GroupsControllerBannedMembersTest extends BaseGroupsControllerTest 
     assertThat(response.getStatus()).isEqualTo(403);
     assertThat(response.hasEntity()).isFalse();
     assertThat(response.getStringHeaders()).containsEntry("x-signal-forbidden-reason", List.of("banned"));
+  }
+
+  @Test
+  public void testGetGroupLogsWhenBanned() {
+    final Group.Builder groupBuilder = group.toBuilder();
+
+    setMockGroupState(groupBuilder);
+    when(groupsManager.getChangeRecords(eq(ByteString.copyFrom(groupPublicParams.getGroupIdentifier().serialize())), isA(Group.class), any(), anyBoolean(), anyBoolean(), anyInt(), anyInt()))
+        .thenReturn(CompletableFuture.completedFuture(List.of(GroupChangeState.newBuilder().setGroupState(groupBuilder).build())));
+    Response response = getGroupLogs(0);
+    assertThat(response.getStatus()).isEqualTo(200);
+    assertThat(response.hasEntity()).isTrue();
+
+    groupBuilder.addMembersBannedBuilder().setUserId(groupBuilder.getMembers(1).getUserId());
+    groupBuilder.removeMembers(1);
+
+    setMockGroupState(groupBuilder);
+    when(groupsManager.getChangeRecords(eq(ByteString.copyFrom(groupPublicParams.getGroupIdentifier().serialize())), isA(Group.class), any(), anyBoolean(), anyBoolean(), anyInt(), anyInt()))
+        .thenReturn(CompletableFuture.completedFuture(List.of(GroupChangeState.newBuilder().setGroupState(groupBuilder).build())));
+    response = getGroupLogs(0);
+    assertThat(response.getStatus()).isEqualTo(403);
+    assertThat(response.hasEntity()).isFalse();
+  }
+
+  private Response getGroupLogs(int fromVersion) {
+    return resources.getJerseyTest()
+        .target("/v1/groups/logs/" + fromVersion)
+        .request(ProtocolBufferMediaType.APPLICATION_PROTOBUF)
+        .header("Authorization", AuthHelper.getAuthHeader(groupSecretParams, AuthHelper.VALID_USER_TWO_AUTH_CREDENTIAL))
+        .get();
   }
 
   private Response getGroupJoinInfoWithPassword(String inviteLinkPasswordString) {
