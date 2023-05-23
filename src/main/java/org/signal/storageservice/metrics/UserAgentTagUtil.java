a005ba33c1f4d0638360e1f91f6e113e4514f668
Ehren Kret <ehren@signal.org>
2023-05-22 23:01:25 -0500

    remove unused code

--- src/main/java/org/signal/storageservice/metrics/UserAgentTagUtil.java
+++ src/main/java/org/signal/storageservice/metrics/UserAgentTagUtil.java
@@ -5,15 +5,7 @@
 
 package org.signal.storageservice.metrics;
 
-import com.vdurmont.semver4j.Semver;
 import io.micrometer.core.instrument.Tag;
-import java.util.EnumMap;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
-import org.signal.storageservice.util.Pair;
-import org.signal.storageservice.util.ua.ClientPlatform;
 import org.signal.storageservice.util.ua.UnrecognizedUserAgentException;
 import org.signal.storageservice.util.ua.UserAgentUtil;
 
@@ -23,21 +15,6 @@ import org.signal.storageservice.util.ua.UserAgentUtil;
 public class UserAgentTagUtil {
 
   public static final String PLATFORM_TAG = "platform";
-  public static final String VERSION_TAG = "clientVersion";
-  static final List<Tag> OVERFLOW_TAGS = List.of(Tag.of(PLATFORM_TAG, "overflow"), Tag.of(VERSION_TAG, "overflow"));
-  static final List<Tag> UNRECOGNIZED_TAGS = List.of(Tag.of(PLATFORM_TAG, "unrecognized"),
-      Tag.of(VERSION_TAG, "unrecognized"));
-
-  private static final Map<ClientPlatform, Semver> MINIMUM_VERSION_BY_PLATFORM = new EnumMap<>(ClientPlatform.class);
-
-  static {
-    MINIMUM_VERSION_BY_PLATFORM.put(ClientPlatform.ANDROID, new Semver("4.0.0"));
-    MINIMUM_VERSION_BY_PLATFORM.put(ClientPlatform.DESKTOP, new Semver("1.0.0"));
-    MINIMUM_VERSION_BY_PLATFORM.put(ClientPlatform.IOS, new Semver("3.0.0"));
-  }
-
-  static final int MAX_VERSIONS = 1_000;
-  private static final Set<Pair<ClientPlatform, Semver>> SEEN_VERSIONS = new HashSet<>();
 
   private UserAgentTagUtil() {
   }
@@ -53,5 +30,4 @@ public class UserAgentTagUtil {
 
     return Tag.of(PLATFORM_TAG, platform);
   }
-
 }
