ab0be3ce58d5c9426edee09e7434312d108162e4
Jon Chambers <63609320+jon-signal@users.noreply.github.com>
2025-04-03 17:18:58 -0400

    Revert "Measure response sizes"
    This reverts commit 2d4d5fcf797a2ad89b5176c4da8d3ce352de0c8b.

--- src/main/java/org/signal/storageservice/metrics/MetricsRequestEventListener.java
+++ src/main/java/org/signal/storageservice/metrics/MetricsRequestEventListener.java
@@ -12,23 +12,18 @@ import org.signal.storageservice.util.UriInfoUtil;
 import org.signal.storageservice.util.ua.ClientPlatform;
 import org.signal.storageservice.util.ua.UnrecognizedUserAgentException;
 import org.signal.storageservice.util.ua.UserAgentUtil;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 /**
  * Gathers and reports request-level metrics.
  */
 public class MetricsRequestEventListener implements RequestEventListener {
 
-  static final String REQUEST_COUNTER_NAME = MetricRegistry.name(MetricsRequestEventListener.class, "request");
-  static final String RESPONSE_BYTES_COUNTER_NAME = MetricRegistry.name(MetricsRequestEventListener.class, "responseBytes");
+  public static final String REQUEST_COUNTER_NAME = MetricRegistry.name(MetricsRequestEventListener.class, "request");
 
   static final String PATH_TAG = "path";
   static final String METHOD_TAG = "method";
   static final String STATUS_CODE_TAG = "status";
   static final String PLATFORM_TAG = "platform";
-
-  private static final Logger log = LoggerFactory.getLogger(MetricsRequestEventListener.class);
 
   private final MeterRegistry meterRegistry;
 
@@ -45,19 +40,16 @@ public class MetricsRequestEventListener implements RequestEventListener {
   public void onEvent(final RequestEvent event) {
     if (event.getType() == RequestEvent.Type.FINISHED) {
       if (!event.getUriInfo().getMatchedTemplates().isEmpty()) {
-        final String path = UriInfoUtil.getPathTemplate(event.getUriInfo());
-        final String method = event.getContainerRequest().getMethod();
-
         Tags tags = Tags.of(
-            PATH_TAG, path,
-            METHOD_TAG, method,
+            PATH_TAG, UriInfoUtil.getPathTemplate(event.getUriInfo()),
+            METHOD_TAG, event.getContainerRequest().getMethod(),
             STATUS_CODE_TAG, String.valueOf(event.getContainerResponse().getStatus()));
 
         final List<String> userAgentValues = event.getContainerRequest().getRequestHeader("User-Agent");
 
         if (userAgentValues != null && !userAgentValues.isEmpty()) {
           try {
-            final ClientPlatform platform = UserAgentUtil.getPlatformFromUserAgentString(userAgentValues.getFirst());
+            final ClientPlatform platform = UserAgentUtil.getPlatformFromUserAgentString(userAgentValues.get(0));
             tags = tags.and(PLATFORM_TAG, platform.name().toLowerCase());
           } catch (UnrecognizedUserAgentException e) {
             tags = tags.and(PLATFORM_TAG, "unknown");
@@ -65,15 +57,6 @@ public class MetricsRequestEventListener implements RequestEventListener {
         }
 
         meterRegistry.counter(REQUEST_COUNTER_NAME, tags).increment();
-
-        final int responseLength = event.getContainerResponse().getLength();
-
-        // Response length can be -1 to indicate "unknown"
-        if (responseLength >= 0) {
-          meterRegistry.counter(RESPONSE_BYTES_COUNTER_NAME, tags).increment(event.getContainerResponse().getLength());
-        } else {
-          log.warn("Unknown response size for {} {}", method, path);
-        }
       }
     }
   }
